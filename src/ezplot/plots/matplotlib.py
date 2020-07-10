5323c81b1eb2e99099301158def68bf9b77c0414
Ubuntu <ubuntu@ip-172-31-3-178.us-west-2.compute.internal>
2020-07-10 01:51:44 +0000

    add more properties to scatter plot

--- src/ezplot/plots/matplotlib.py
+++ src/ezplot/plots/matplotlib.py
@@ -42,18 +42,87 @@ def make_occur_plot(X, Y, df, force_dtype={}):
     return heatmap(x, y, counts)
     
 def make_scatter_plot(X, Y, df, force_dtype={}, **params):
-    fig, ax = plt.subplots()
-    ax.scatter(df[X], df[Y])
-    ax.set_xlabel(X)
-    ax.set_ylabel(Y)
-    if 'title' in params:
-        ax.set_title(params['title'])
+    fig, ax = plt.subplots(
+        figsize=(params['Figsize_x'], params['Figsize_y'])
+    )
+    ax.scatter(df[X], df[Y],
+               s=params['markersize'],
+               alpha=params['alpha'])
+    ax.set_xlabel(params['xlabel'])
+    ax.set_ylabel(params['ylabel'])
+    if 'Title' in params:
+        ax.set_title(params['Title'])
+    if 'xmin' in params:
+        ax.set_xlim(left=params['xmin'])
+    if 'xmax' in params:
+        ax.set_xlim(right=params['xmax'])
+    if 'ymin' in params:
+        ax.set_ylim(bottom=params['ymin'])
+    if 'ymax' in params:
+        ax.set_ylim(top=params['ymax'])
+    if 'half_spine' in params:
+        ax.spines["top"].set_visible(not params['half_spine'])  
+        ax.spines["right"].set_visible(not params['half_spine'])
     plt.show(fig)
+    if params['save']:
+        fig.savefig('saved.png')
 
 def make_interactive_scatter_plot(X, Y, df, force_dtype={}):
+    def plot(**params):
+        make_scatter_plot(X, Y, df, force_dtype, **params)
     widgets.interact(
-        lambda Title: make_scatter_plot(X, Y, df, force_dtype, title=Title),
-        Title="Title",
+        plot,
+        Title="",
+        xlabel=X,
+        ylabel=Y,
+        xmin=widgets.FloatSlider(
+            min=df[X].min() - (df[X].max() - df[X].min())/10,
+            max=df[X].min(),
+            value=df[X].min() - (df[X].max() - df[X].min())/20,
+            step=max(df[X].max() - df[X].min(),1)/100,
+        ),
+        xmax=widgets.FloatSlider(
+            min=df[X].max(),
+            max=df[X].max() + (df[X].max() - df[X].min())/10,
+            value=df[X].max() + (df[X].max() - df[X].min())/20,
+            step=(df[X].max() - df[X].min())/100,
+        ),
+        ymin=widgets.FloatSlider(
+            min=df[Y].min() - (df[Y].max() - df[Y].min())/10,
+            max=df[Y].min(),
+            value=df[Y].min()-(df[Y].max() - df[Y].min())/20,
+            step=(df[Y].max() - df[Y].min())/100,
+        ),
+        ymax=widgets.FloatSlider(
+            min=df[Y].max(),
+            max=df[Y].max() + (df[Y].max() - df[Y].min())/10,
+            value=df[Y].max()+(df[Y].max() - df[Y].min())/20,
+            step=(df[Y].max() - df[Y].min())/100,
+        ),
+        Figsize_x=widgets.IntSlider(
+            min=1,
+            max=10,
+            value=4,
+        ),
+        Figsize_y=widgets.IntSlider(
+            min=1,
+            max=10,
+            value=3,
+        ),
+        markersize=widgets.IntSlider(
+            min=1,
+            max=100,
+            value=10,
+            step=1,
+        ),
+        alpha=widgets.FloatSlider(
+            min=0,
+            max=1,
+            value=1,
+            step=0.01,
+        ),
+        half_spine=True,
+        save=False,
     )
 def make_line_plot(X, Y, df, force_dtype={}, **params):
     fig, ax = plt.subplots()
